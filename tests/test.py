6de2b938f023fa763b8cd2c1eea572c2cd99763d
Seungha Son <seungha.son@samsung.com>
2020-01-21 16:29:13 +0900

    Adjust unittest

---
+++ tests/test.py
@@ -0,0 +1,14 @@
+import os
+import unittest
+import ccodegen as cg
+
+class TestCodeGen(unittest.TestCase):
+    def test_ifndef(self):
+        tmp = cg.Ifndef('__TEST_H__')
+        self.assertEqual(str(tmp), '#ifndef __TEST_H__\n')
+
+    def test_endif(self):
+        self.assertEqual(str(cg.Endif()), '#endif')
+
+if __name__ == '__main__':
+    unittest.main()
