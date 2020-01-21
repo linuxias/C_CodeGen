b12dfad0dbee67c4720b1042421c09c6a3b82d9f
Seungha Son <seungha.son@samsung.com>
2020-01-21 14:49:14 +0900

    Adjust packaging struct

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='ccodegen',
+    version='0.1',
+    description="C code generator",
+    author="Seungha Son",
+    author_email="linuxias@gmail.com",
+    license="MIT",
+    url="https://github.com/linuxias/C_CodeGen",
+    keywords=["C", "Generator"],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
